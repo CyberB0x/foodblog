c71b1e7aa40d59615b3ea16d40a59b1a6418fb82
Arslon <arslonbekefsun09@gmail.com>
2026-03-08 23:29:57 +0300

    foodblog day-1

---
+++ recipes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image = models.ImageField(upload_to='recipes/images/')
+    video_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
