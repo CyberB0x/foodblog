3ff4923d7d5b0e29adaec3c524b1c8857e150052
Arslon <arslonbekefsun09@gmail.com>
2026-03-11 14:55:54 +0300

    food blog day 2

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
-# Create your views here.
+
+def home(request):
+    recipes = Recipe.objects.all().order_by("-created_at")
+    return render(request, "home.html", {"recipes": recipes})
+
+def recipe_detail(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, "recipe_detail.html", {"recipe": recipe})
